86a9d8b99d344527cd49eb36e377f9c127a8047c
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2022-07-01 08:52:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lista de Presença
+
+### Esse projeto foi realizado no curso [Fundamentos do ReactJS](https://app.rocketseat.com.br/discover/course/especializar-react) da Rocketseat
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,65 @@
+import "./styles.css";
+import { Card } from "../../components/Card";
+import { useState, useEffect } from "react";
+
+export function Home() {
+  const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({ name: "", avatar: "" });
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+
+    if (newStudent.name !== '' || newStudent.name !== null) {
+      setStudents((prevState) => [...prevState, newStudent]);
+    } else {
+      console.log('Erro')
+    }
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/washinRibeiro")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong> {user.name} </strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button
+        type="button"
+        onClick={handleAddStudent}
+      >
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
+    </div>
+  );
+}
